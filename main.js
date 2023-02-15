6cbdd6da0e4682341b2a2faefd9706ccd3ab11d0
Laverna-101 <121871747+Laverna-101@users.noreply.github.com>
2023-02-15 23:12:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function quoteGenerator(){
     
     if(days == 16){
         quote.innerHTML = "Happy 5.7 Monthsarry Langga";
-        quote.style.transform = "translate" + "(" + -50 + "%" + "," +  -180 + "%" + ")";
+        quote.style.transform = "translate" + "(" + -50 + "%" + "," +  -190 + "%" + ")";
         carousel.style.display = "block";
         message.style.display = "block";
         dayAndNightBackground(iPic);
